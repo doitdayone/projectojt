98a5e1dad059ca45b0c616520bcd7803644e9576
doitdayone <dorayaki22t52gmail.com>
2024-03-21 08:47:04 +0700

    tracking order

--- src/main/java/com/example/projectojt/repository/OrderRepository.java
+++ src/main/java/com/example/projectojt/repository/OrderRepository.java
@@ -1,7 +1,16 @@
 package com.example.projectojt.repository;
 
 import com.example.projectojt.model.Order;
+import com.example.projectojt.model.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    @Query(value = "SELECT orders.* FROM orders JOIN address ON orders.addressId = address.addressId WHERE orders.status = ?1 AND address.userID = ?2", nativeQuery = true)
+    ArrayList<Order> findOrdersByUserIdAndStatus(int user_id, String status);
+
+    Order findById(long id);
 }
