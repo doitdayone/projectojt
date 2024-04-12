2188cb98b3ec500addfbd47332e54d8d17a5281c
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-04-12 14:39:19 +0700

    Done filter product type page

--- src/main/java/com/example/projectojt/controller/FeedbackController.java
+++ src/main/java/com/example/projectojt/controller/FeedbackController.java
@@ -1,12 +1,16 @@
 package com.example.projectojt.controller;
 
 import com.example.projectojt.model.Feedback;
+import com.example.projectojt.model.User;
 import com.example.projectojt.repository.FeedbackRepository;
 import com.example.projectojt.repository.ProductRepository;
 import com.example.projectojt.repository.UserRepository;
 import com.example.projectojt.service.ProductService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +18,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/EcommerceStore")
@@ -30,7 +36,41 @@ public class FeedbackController {
     private ProductService productService;
 
     @GetMapping("/do-feedback")
-    public String reviewProducts(Model model, HttpSession session) {
+    public String reviewProducts(Model model, Authentication authentication,
+                                 HttpSession session) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+
+            if (principal instanceof UserDetails userDetails) {
+                // Standard UserDetails case
+                String email = userDetails.getUsername();
+                model.addAttribute("user_email", email);
+                User user = userRepository.findByEmail(email);
+                model.addAttribute("userRepository", userRepository);
+                int userid = user.getUserID();
+                model.addAttribute("user_id", userid);
+                session.setAttribute("user_id", userRepository.findByEmail(email).getUserID());
+                if (user.getRoles().equals("ADMIN"))
+                    return "redirect:/admin";
+            } else if (principal instanceof OAuth2User oAuth2User) {
+                // get user_email when sign in with google or facebook
+                Map<String, Object> attributes = oAuth2User.getAttributes();
+                model.addAttribute("user_email",
+                        attributes.get("email"));
+
+                if(!userRepository.existsByEmail((String) attributes.get("email"))){
+                    var user =  User.builder().userName((String) attributes.get("name"))
+                            .email((String) attributes.get("email")).password("").verified(true).roles("USER").build();
+                    userRepository.save(user);
+                    ;
+                }
+                session.setAttribute("user_id", userRepository.findByEmail((String) attributes.get("email")).getUserID());
+                model.addAttribute("userRepository", userRepository);
+
+            } else {
+                return "error";
+            }
+        }
         model.addAttribute("user_email", userRepository.findByUserID((int) session.getAttribute("user_id")).getEmail());
         model.addAttribute("products", productRepository.findProductsToFeedbackByUser((int) session.getAttribute("user_id")));
         return "feedback";
